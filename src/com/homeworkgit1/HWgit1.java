aca56207031126fc56d9d2d6ffe8bdd016f579cc
anako <hope.love.faith87@gmail.com>
2021-08-12 17:42:55 -0400

    third commit

--- src/com/homeworkgit1/HWgit1.java
+++ src/com/homeworkgit1/HWgit1.java
@@ -3,7 +3,7 @@ package com.homeworkgit1;
 public class HWgit1 {
 
 	public static void main(String[] args) {
-		for(int i=0; i<=10; i++) {
+		for(int i=0; i<=100; i++) {
 			System.out.println(i);
 		}
 	
